b87f1dd0219315b765166b2f8fe1735621905e8a
Stewart <stewartjumbe@gmail.com>
2022-01-20 16:18:11 +0000

    added a new PaintCostCalculation Class)

---
+++ src/com/softwareinstitute/sjumbe/PaintCostCalculation.java
@@ -0,0 +1,43 @@
+package com.softwareinstitute.sjumbe;
+
+import java.util.Scanner;
+
+public class PaintCostCalculation {
+    //class containing methods for calculating the cost of painting a house
+
+    //Attributes
+
+    //int paintPotAreaCoverage = 10; // m2
+    //float costOfPaintPotInGBP = 5.76f;
+    int timeToPaintARoomInHrs = 4;
+    float hourlyRateInGBP = 75.50f;
+    float heightOfRoomWallInMeters= 0;
+    float lengthOfRoomWallInMeters = 4f ;
+    int numberOfRoomsInTheHouse = (int)((Math.random()*100) +1) ;
+    float numberOfWallsInRoom = 4f;
+
+    //blank constructor
+    public PaintCostCalculation() {
+    };
+
+
+    //Scanner costOfPaintPotInGBPInput = new Scanner(System.in);
+    //double costOfPaintPotInGBP = Double.parseDouble(costOfPaintPotInGBPInput.nextLine());
+
+    //Method for calculating the amount of paint needed to paint a room.
+    public  void calculatingNumberOfPaintPotsNeeded(){
+
+        Scanner paintPotAreaCoverageInput = new Scanner(System.in);
+        System.out.println("Please enter what area your paint covers in m2: ");
+        int paintPotAreaCoverage = Integer.parseInt(paintPotAreaCoverageInput.nextLine()); //needed to parse string into int
+
+        float areaOfWallInMetersSqr = heightOfRoomWallInMeters * lengthOfRoomWallInMeters ;
+
+        float totalWallAreaInHouseInMetersSqr = areaOfWallInMetersSqr * numberOfWallsInRoom * numberOfRoomsInTheHouse;
+
+        float numberOfPaintPotsNeeded =totalWallAreaInHouseInMetersSqr/paintPotAreaCoverage;
+        int numberOfPaintPotsNeededRoundedUp = (int)Math.ceil(numberOfPaintPotsNeeded);
+        System.out.println("The number of Paint Pots Needed is: " + numberOfPaintPotsNeededRoundedUp);
+
+    }
+}
